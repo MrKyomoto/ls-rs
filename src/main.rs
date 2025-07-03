478a2fe633ce205047b7ad66294186606107fea7
kyomoto <2028566723@qq.com>
2025-07-03 13:20:44 +0800

    src: 更好的显示文件元数据

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+use chrono::{DateTime, Utc};
 use clap::Parser;
 use owo_colors::OwoColorize;
 use strum::Display;
+use tabled::{settings::{object::{Columns, Rows}, Color, Style}, Table, Tabled};
 use std::{fs, path::{Path, PathBuf}};
 
 #[derive(Debug,Display)]
-enum entryType {
+enum EntryType {
     File,
     Dir,  
 }
 
 // store meta data
-#[derive(Debug)]
-struct fileEntry{
+#[derive(Debug, Tabled)]
+struct FileEntry{
+    #[tabled{rename="Name"}]
     name:String,
-    e_type:entryType, 
+    #[tabled{rename="Type"}]
+    e_type:EntryType, 
+    #[tabled{rename="Size B"}]
     len_bytes:u64,
+    #[tabled{rename="Modified"}]
     modified:String,
 }
 
@@ -34,9 +40,14 @@ fn main() {
 
     if let Ok(exist) = fs::exists(&path){
         if exist {
-            for file in getFiles(&path){
-                println!("{:?}",file);
-            }
+            let files = get_files(&path);
+            let mut table = Table::new(files);
+            table.with(Style::rounded());
+            table.modify(Columns::first(), Color::FG_BRIGHT_CYAN);
+            table.modify(Columns::one(2), Color::FG_BRIGHT_MAGENTA);
+            table.modify(Columns::one(3), Color::FG_BRIGHT_YELLOW);
+            table.modify(Rows::first(), Color::FG_BRIGHT_GREEN);
+            println!("{}",table);
         }
         else {
             println!("{}","Path does not exist".red());
@@ -48,31 +59,38 @@ fn main() {
     // println!("{}",path.display());
 }
 
-fn getFiles(path:&Path) -> Vec<fileEntry>{
+fn get_files(path:&Path) -> Vec<FileEntry>{
     let mut data = Vec::default();
     if let Ok(read_dir) = fs::read_dir(path){
         for entry in read_dir{
             if let Ok(file) = entry{
-                if let Ok(meta) = fs::metadata(&file.path()){
-                    data.push(
-                        fileEntry { 
-                            name: file.file_name()
-                                .into_string()
-                                .unwrap_or("unknown name".into()),
-                            len_bytes: meta.len(),
-                            modified: " ".to_string(),
-                            e_type: if meta.is_dir(){
-                                entryType::Dir
-                            } 
-                            else{
-                                entryType::File
-                            },
-                    }
-                );
- 
-                }
+                map_data(file, &mut data);
            }
         }
     }
     data
+}
+
+fn map_data(file: fs::DirEntry,data: &mut Vec<FileEntry>){
+    if let Ok(meta) = fs::metadata(&file.path()){
+        data.push(
+            FileEntry { 
+                name: file.file_name()
+                    .into_string()
+                    .unwrap_or("unknown name".into()),
+                len_bytes: meta.len(),
+                e_type: if meta.is_dir(){
+                    EntryType::Dir
+                }else{
+                    EntryType::File
+                },
+                modified: if let Ok(modified) = meta.modified(){
+                    let date:DateTime<Utc> = modified.into();
+                    format!("{}",date.format("%a %b %e %Y"))
+                }else{
+                    String::default()
+                },
+        }
+    );
+    }
 }
